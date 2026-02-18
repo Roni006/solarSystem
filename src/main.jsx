253abf2ba7c54007ef95af938ea504bb7e269f69
Anik Islam <anik54908@gmail.com>
2026-02-18 14:53:57 +0600

    added animated cursot

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import AnimatedCursor from "react-animated-cursor";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <AnimatedCursor
+      innerSize={10}
+      outerSize={10}
+      color="255,0,0"
+      outerAlpha={0.5}
+      innerScale={1.5}
+      outerScale={4.5}
+    />
     <App />
   </StrictMode>,
-)
+);
